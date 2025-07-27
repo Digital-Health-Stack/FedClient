9cee4c9756bdd263910d3932f16d9bbc0cb07081
harshbaid-13 <harshbaid.ind@gmail.com>
2025-07-27 15:07:13 +0530

    env fix

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -1,31 +1,15 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const environmentVariables = [
-  "REACT_APP_SERVER_BASE_URL",
-  "REACT_APP_PRIVATE_SERVER_BASE_URL",
-  "REACT_APP_SERVER_DATASET_OVERVIEW_PATH",
-  "REACT_APP_GET_TASKS_WITH_DATASET_ID",
-  "REACT_APP_VIEW_RECENT_UPLOADS_URL",
-  "REACT_APP_RAW_DATASETS_ENDPOINT",
-  "REACT_APP_RAW_DATASET_RENAME_ENDPOINT",
-  "REACT_APP_DELETE_RAW_ENDPOINT",
-  "REACT_APP_RAW_OVERVIEW_PATH",
-  "REACT_APP_PROCESSED_DATASETS_ENDPOINT",
-  "REACT_APP_PROCESSED_DATASET_RENAME_ENDPOINT",
-  "REACT_APP_DELETE_PROCESSED_ENDPOINT",
-  "REACT_APP_PROCESSED_OVERVIEW_PATH",
-  "REACT_APP_INITIATE_MODEL_FOR_TRAINING",
-  "REACT_APP_EXECUTE_TRAINING_ROUND",
-  "REACT_APP_CREATE_NEW_DATASET_URL",
-  "REACT_APP_PREPROCESS_DATASET_URL",
-  "REACT_APP_PRIVATE_SERVER_URL",
-];
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const processEnv = {};
-  environmentVariables.forEach((key) => (processEnv[key] = env[key]));
+  // Load only environment variables that start with REACT_APP_
+  Object.keys(env).forEach((key) => {
+    if (key.startsWith('REACT_APP_')) {
+      processEnv[key] = env[key];
+    }
+  });
 
   return {
     define: {
